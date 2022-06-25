7ad591fba5890cbfe37d5f4e846d0f58eb5cee8b
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-06-25 16:44:11 +0200

    feat: install stitches (add basic theme)

---
+++ stitches.config.ts
@@ -0,0 +1,13 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled } = createStitches({
+    theme: {
+        colors: {
+            absent: '#787c7e',
+            present: '#c9b458',
+            error: '#bb3429',
+            correct: '#6aaa64',
+            background: '#d3d6da'
+        }
+    }
+})
