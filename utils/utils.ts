51bc09919ca65f8a2914610d2a05d732475599f8
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-06-28 20:47:41 +0200

    feat: add generate matrix util

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,14 @@
-import { MAX_POKEMON_ID } from '../constants/pokemon';
+import { MAX_POKEMON_ID } from "../constants/pokemon";
+import { Letter, LetterStatus } from "../models/pokedle";
 
-export const generatePokemonId = () => Math.ceil(Math.random() * MAX_POKEMON_ID)
+export const generatePokemonId = () =>
+  Math.ceil(Math.random() * MAX_POKEMON_ID);
+
+export const generateLetters = (
+  pokemonNameLength: number,
+  tries: number
+): Letter[][] =>
+  Array.from<Letter[]>({ length: tries }).map(() => Array.from<Letter>({length: pokemonNameLength}).fill({
+    value: '',
+    status: LetterStatus.EMPTY
+  }));
