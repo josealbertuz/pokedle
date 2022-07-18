dddae59f647626f73c829ad613d36637397240ca
Jose Antonio Albértuz <josealbertuz@gmail.com>
2022-07-18 10:55:31 +0200

    feat: add utils to get random pokemon name

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,18 @@
-import { MAX_POKEMON_ID } from "../constants/pokemon";
 import { Letter, LetterStatus } from "../models/pokedle";
 
-export const generatePokemonId = () =>
-  Math.ceil(Math.random() * MAX_POKEMON_ID);
+export const getRandomItemFromArray = <T>(array: T[]) =>
+  array[Math.ceil(Math.random() * array.length)];
+
+export const getRandomPokemonName = (pokemonNames: string[], maxLength: number): string => {
+
+  const pokemonName = getRandomItemFromArray(pokemonNames)
+
+  if (pokemonName.length === maxLength) return pokemonName
+
+  const filteredPokemonNames = pokemonNames.filter(pokemon => pokemon !== pokemonName)
+
+  return getRandomPokemonName(filteredPokemonNames, maxLength)
+}
 
 export const generateLetters = (
   pokemonNameLength: number,
@@ -10,5 +20,6 @@ export const generateLetters = (
 ): Letter[][] =>
   Array.from<Letter[]>({ length: tries }).map(() => Array.from<Letter>({length: pokemonNameLength}).fill({
     value: '',
-    status: LetterStatus.EMPTY
+    status: LetterStatus.EMPTY,
+    animate: false
   }));
